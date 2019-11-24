4935475bf0ed66bc3aaa7cdcccb0647e45ae6f8f
abubakrdanish <55455591+abubakrdanish@users.noreply.github.com>
2019-11-24 16:51:53 +0500

    Add files via upload

---
+++ alpha bit.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, letter As String
+        Dim counter, alphabit, Nonalphabit As Integer
+
+        str1 = ""
+        letter = ""
+        counter = 0
+        alphabit = 0
+        Nonalphabit = 0
+
+
+        Console.Write("input string ")
+        str1 = Console.Read
+
+        For counter = 1 To Len(str1)
+            letter = Mid(str1, counter, 1)
+            If LCase(letter) < "a" And LCase(letter) > "z" Then
+                Nonalphabit = Nonalphabit + 1
+            Else
+                alphabit = alphabit + 1
+
+            End If
+
+
+
+
+        Next
+        Console.WriteLine(Nonalphabit)
+        Console.WriteLine(alphabit)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
