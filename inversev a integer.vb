10daecf52f9623da2c1a84a6a44a1955d19c96c9
abubakrdanish <55455591+abubakrdanish@users.noreply.github.com>
2019-11-26 18:51:45 +0500

    invrse a integer

---
+++ inversev a integer.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim count As Integer
+        Dim ch As Char
+        Dim str, rstr As String
+
+        count = 0
+        ch = ""
+
+
+
+        str = ""
+        rstr = ""
+
+
+        Console.WriteLine("enter a string:")
+        For count = Len(str) To 1 Step -1
+            ch = Mid(str, count, 1)
+            rstr = rstr & ch
+
+
+
+        Next count
+        Console.Write("rstr")
+        Console.ReadLine()
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+End Module
